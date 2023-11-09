12fcb0ed396cf5238a5b3954b354c2a7836a669c
时光SHG <17777552458@163.com>
2023-11-09 15:14:35 +0800

    配置request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,38 +1,67 @@
-// import axios from 'axios';
-// // import store from '@/store/index'
+import axios from "axios";
+import { MessageBox, Message } from "element-ui";
+import { getLocalStorage, removeLocalStorage } from "./";
 
-// const instance = axios.create({
-//     baseURL: 'http://xue.cnkdl.cn:23683',
-//     timeout: 8000,
-//     responseType: 'json',
-//     headers:{
-//         'Content-Type': 'application/json;charset=utf-8',
-//     }
-// })
+export default (config) => {
 
-// // 请求拦截
-// instance.interceptors.request.use(
-//     config => {
-//         // 处理请求数据
-//         // config.headers.Authorization = store.state.token;
-//         return config;
-//     },
-//     error => {
-//         // 处理请求错误
-//         return Promise.reject(error);
-//     }
-// )
+    const baseURL = process.env.NODE_ENV === 'production' ? process.env.VITE_APP_BASE_API : '/api';
+    // 超时
+    const timeout = 5000;
+    // 创建axios实例
+    const service = axios.create({
+        baseURL,
+        timeout
+    })
 
-// // 响应拦截
-// instance.interceptors.response.use(
-//     response => {
-//         console.log(response,"response")
-//         return response;
-//     },
-//     error => {
-//         console.log(`请求错误：${error}`);
-//         return Promise.reject(error)
-//     }
-// )
+    // requist拦截器
+    service.interceptors.request.use((config) => {
+        config.headers["Authorization"] = getLocalStorage('shg_token') || "";
+        config.headers["Content-Type"] = config.headers["Content-Type"] || "application/json";
+        if (config.type == "file") {
+            config.headers["content-type"] = "application/multipart/form-data";
+        } else if (config.type == "form") {
+            config.headers["Content-type"] = "application/x-www-form-urlencoded";
+        }
+        if (config.method && config.method.toLowerCase() === "get") {
+            config.data = true;
+        }
+        return config;
+    },
+        (error) => {
+            return Promise.reject(error);
+        });
 
-// export default instance;
+    // reponst拦截器
+    service.interceptors.response.use((response) => {
+        const code = response.code;
+        if (code === 401) {
+            MessageBox.confirm("登录状态已过期，您可以继续留在该页面，或者重新登录", "系统提示", {
+                confirmButtonText: "重新登录",
+                cancelButtonText: "取消",
+                type: "warning"
+            }).then(async () => {
+                // 调用退出登录接口
+                removeLocalStorage('shg_token')
+            });
+        } else if (code !== 200) {
+            Message({
+                message: response.data.msg,
+                type: "error",
+                duration: 5 * 1000
+            });
+            return Promise.reject("error");
+        } else {
+            return response.data;
+        }
+    },
+        (error) => {
+            Message({
+                message: error.message,
+                type: "error",
+                duration: 5 * 1000
+            });
+            return Promise.reject(error);
+        })
+
+        return service(config);
+};
